f1baf5b3908374a3922d27da398c510ee49e3b4c
giuliamari04 <giuliamari04@gmail.com>
2023-10-10 18:00:58 +0200

    tasto reset

--- js/script.js
+++ js/script.js
@@ -5,25 +5,34 @@
 const buttonGenera = document.getElementById('genera');
 const buttonreset = document.getElementById('reset');
 
+
+buttonreset.addEventListener('click', function () {
+    const nomeCognomeInput = document.getElementById('nomeCognome');
+    const kmInput = document.getElementById('km');
+    const etaSelect = document.getElementById('eta');
+
+    nomeCognomeInput.value = '';
+    kmInput.value = '';
+    etaSelect.value = '';
+
+   
+
+});
+
 buttonGenera.addEventListener('click', function () {
     let nome = document.getElementById('nomeCognome').value;
     let percorso = parseInt(document.getElementById('km').value);
     let etaPasseggero = document.getElementById('eta').value;
 
-    let risultatonome = document.querySelector('.risultato-nome');
-    let risultatoeta = document.querySelector('.risultato-eta');
-    let risultatoprezzo = document.querySelector('.risultato-prezzo');
-    let risultatoVagone = document.querySelector('.risultato-vagone');
-    let risultatoCP = document.querySelector('.risultato-CP');
-
-
-    
+    let risultatonome = document.querySelector('.biglietto');
 
     console.log('nome:',nome,'perorso:',percorso,'ed età:',etaPasseggero);
 
     let prezzoBiglietto = 0.21;
     let prezzoPercorso = prezzoBiglietto * percorso;
-   
+   if(nome ==='' || percorso === '' || etaPasseggero ===''){
+    alert('errore dati');
+   }else{
     
     
     if(etaPasseggero ==='minorenne'){
@@ -50,35 +59,40 @@ buttonGenera.addEventListener('click', function () {
 
       let randomNumberCP = getRandomNumber(10000, 99999);
       console.log(randomNumberVagone);
-      
-    
-    risultatonome.innerHTML =
-     `
-        ${nome} 
-     `;
-      
-     risultatoeta.innerHTML =
-     `
-        ${etaPasseggero} 
-     `;
 
-     risultatoVagone.innerHTML =
+     risultatonome.innerHTML =
      `
-        ${randomNumberVagone} 
-     `;
+     <h2 class="text-uppercase text-center">il tuo biglietto</h2>
+     <table class="m-2 border-1">
+     <tr>
+         <th class="my-bg-first-cell">
+             <h3>nome passeggero</h3>
+             <div> ${nome}</div></th>
+         <th>
+             <h3>Offerta</h3>
+         </th>
+         <th>
+             <h3>Carrozza</h3>
+         </th>
+         <th>
+             <h3>codice CP</h3>
+         </th>
+         <th>
+             <h3>Costo biglietto</h3>
+         </th>
+     </tr>
 
-     risultatoCP.innerHTML =
-     `
-        ${randomNumberCP} 
-     `;
-
-     risultatoprezzo.innerHTML =
-     `
-        ${prezzoFormattato} 
-     `;
-      console.log('Prezzo per utente:', risultatoprezzo);
+     <tr>
+         <th class="my-bg-first-cell"></th>
+         <th>${etaPasseggero}</th>
+         <th >${randomNumberVagone}</th>
+         <th > ${randomNumberCP}</th>
+         <th >${prezzoFormattato}€</th>
+     </tr>
+ </table>
+ `;
     
-    
+   }
 });
 
 
